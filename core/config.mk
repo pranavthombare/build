414acbd2c702e9d84e1bc31c8314fcde9d21e604
theringmaster <gzroms@hotmail.com>
2017-12-07 14:06:02 -0500

    Remove assert references
    Change-Id: I065ccca638ed889ee867ae19c0bf8e10d99ec958

--- core/config.mk
+++ core/config.mk
@@ -972,7 +972,7 @@ endif
 ifneq ($(LEGENDROM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/aosp/sepolicy/common/sepolicy.mk)
+#$(eval include vendor/aosp/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
